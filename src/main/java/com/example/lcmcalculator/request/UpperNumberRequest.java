23bc02df6d9521f0be81394b5c5e835fe375364f
Busra Koc <koccbusra@gmail.com>
2023-06-13 11:05:22 +0200

    UpperNumberRequest is updated

--- src/main/java/com/example/lcmcalculator/request/UpperNumberRequest.java
+++ src/main/java/com/example/lcmcalculator/request/UpperNumberRequest.java
@@ -1,12 +1,12 @@
 package com.example.lcmcalculator.request;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpperNumberRequest {
     Integer upperNumber;
 }
